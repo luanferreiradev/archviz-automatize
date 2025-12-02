25b4e5e0928b50183776170b3e705ad860e8e7a5
Luan Lara <122845430+luanferreiradev@users.noreply.github.com>
2025-12-02 10:08:12 -0300

    Added list project method and improved create folder structure

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import com.devandcode.files.FilesUtils;
 import com.devandcode.files.reader.BatchRead;
 import com.devandcode.files.writer.BatchWrite;
 import com.devandcode.service.FolderStructureService;
 import com.devandcode.service.RenderScriptService;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,13 +24,14 @@ public class Main {
     }
 
     public static void chooseOperation(Scanner sc) {
-        System.out.println("Inform the operation: (1) Create Render Script; (2) Create Folder Structure; (3) Clear Logs;");
+        System.out.println("Inform the operation: (1) Create Render Script; (2) Create Folder Structure; (3) Clear Logs; (4) List Projects;");
         int option = sc.nextInt();
         sc.nextLine();
         switch (option) {
             case 1 -> createRenderScript(sc);
             case 2 -> createFolderStructure(sc);
             case 3 -> clearLogs(sc);
+            case 4 -> listProjects(sc);
         }
     }
 
@@ -46,12 +51,31 @@ public class Main {
     }
 
     public static void createFolderStructure(Scanner sc) {
-        List<String> subFolders = List.of("Imagens", "Referências", "Blocos", "Proxys", "Xrefs");
+        Properties appProperties = new Properties();
+        Path appPropertiesPath = Path.of("resources").resolve("app.properties");
+        try {
+            appProperties.load(Files.newInputStream(appPropertiesPath));
+        } catch (IOException e) {
+            throw new RuntimeException("It was not possible to read the file: " + appPropertiesPath, e);
+        }
+        List<String> subFolders = Arrays.asList(appProperties.getProperty("subfolders").split(","));
         System.out.print("Inform the folder: ");
         String rootPath = sc.nextLine();
         FolderStructureService folderStructureService = new FolderStructureService(
                 Path.of(rootPath), "skp", subFolders);
         folderStructureService.create();
+    }
+
+    public static void listProjects(Scanner sc) {
+        System.out.print("Inform the folder: ");
+        Path rootPath = Path.of(sc.nextLine());
+        var projects = FilesUtils.listProjects(rootPath);
+        int i = 0;
+        NumberFormat numberFormat = new DecimalFormat("00");
+        for (String project : projects) {
+            System.out.println(numberFormat.format(i) + " - " + project);
+            i++;
+        }
     }
 }
 
--- src/com/devandcode/files/FilesUtils.java
+++ src/com/devandcode/files/FilesUtils.java
@@ -1,9 +1,14 @@
 package com.devandcode.files;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FilesUtils {
     public static List<String> cleanText(List<String> lines) {
@@ -21,5 +26,18 @@ public class FilesUtils {
         var fileCompleteName = path.getFileName().toString();
         var dotPosition = fileCompleteName.lastIndexOf(".");
         return path.getFileName().toString().substring(0, dotPosition);
+    }
+
+    public static List<String> listProjects(Path rootPath) {
+
+        try (Stream<Path> stream = Files.list(rootPath)) {
+            return stream.filter(Files::isDirectory)
+                    .filter(file -> !file.getFileName().toString().contains("00 - Render Script"))
+                    .map(file -> file.getFileName().toString())
+                    .toList();
+
+        } catch (IOException e) {
+            throw new RuntimeException("It was not possible to list the projects", e);
+        }
     }
 }
